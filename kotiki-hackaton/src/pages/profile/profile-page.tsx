767ef32cff71e48fe33684cd047b90d60ce43ac9
Alexey.Krokhalev <Alexey.Krokhalev@urfu.me>
2024-04-06 13:51:11 +0500

    profile page layout added
    Надо доделать и разбить на компоненты

--- kotiki-hackaton/src/pages/profile/profile-page.tsx
+++ kotiki-hackaton/src/pages/profile/profile-page.tsx
@@ -1,8 +1,9 @@
-import { Container, Row, Col} from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
 import { Helmet } from "react-helmet-async"
 import './side-bar.css'
+import './info-panel.css'
 
-
+import vacancy_discription from "../../data/text"
 
 // Компонент главной страницы
 export default function ProfilePage(): JSX.Element {
@@ -11,35 +12,85 @@ export default function ProfilePage(): JSX.Element {
             <Helmet>
                 <title>Profile</title>
             </Helmet>
+            <main className="">
+            <div className="container d-flex">
+                <div className="row flex-fill">
+                    <div className="col-3 side-bar text-center">
 
-            <div className="container">
-                <div className="row ">
-                <div className="col-4 side-bar text-center">
-                    
-                    <div>
-                    <img src ="../../photos/kotik.png"></img>
+                        <div>
+                            <img src="'./img/cat.svg'"></img>
+                        </div>
+                        <div>
+                            <h4>Шешукова Арина Андреевна</h4>
+                        </div>
+                        <div>
+                            <p>Крестьянское ополочение</p>
+                        </div>
                     </div>
-                    <div>
-                    <h4>Шешукова Арина Андреевна</h4>
-                    </div>
-                    <div>
-                    <p>Крестьянское ополочение</p>
-                    </div>
-                </div>
-                
 
-                <div className="col-8">
-                     
-                     <div className="card">
-                            <h1>
-                                Header
-                            </h1>
-                     </div>
 
-                </div>
+                    <div className="col-8 rows">
+                        <div className="row">
+                        <h3 className="title">Мои отклики</h3>
+                            <div className="card col">
+                                <div className="row">
+                                    
+                                    <div className="col-8">
+                                        <h5>
+                                            Разработчик 1С
+                                        </h5>
+                                        <p>Всего этапов: 3</p>
+                                        <p>Текущий этап: 2 — тестовое задание</p>
+                                        <p>Статус: ожидает проверки</p>
+                                        <button className="btn btn-outline-danger">BUTTON</button>
+                                    </div>
+                                    <div className="col">
+                                        <div className="progres-circle">
+                                            <div className="progress-percent mx-auto">33%</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="card col">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <h4>
+                                            Разработчик 1С
+                                        </h4>
+                                        <p>Всего этапов: 3</p>
+                                        <p>Текущий этап: 2 — тестовое задание</p>
+                                        <p>Статус: ожидает проверки</p>
+                                        <button className="btn btn-outline-danger">BUTTON</button>
+                                    </div>
+                                    <div className="col">
+                                        <div className="progres-circle">
+                                            <div className="progress-percent mx-auto">33%</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row info-panel gx-2">
+                            <div>
+                            <ul className="menu-panel row text-center">
+                                <li className="col active">Описание</li>
+                                <li className="col">Прогресс</li>
+                                <li className="col">Чат с рекрутерами</li>
+                            </ul>
+                            </div>
+
+                          <div className="discription">
+                                {vacancy_discription}
+                          </div>
+
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
-            
+            </main>
         </>
     )
 }
